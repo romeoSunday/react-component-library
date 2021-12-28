84f9cb175d7823d7880d59949498800033dfe4ed
Romeo <romeo.lombardelli@sundayapp.com>
2021-12-28 09:43:51 +0100

    chore: init repo

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 10px;
+  background-color: red;
+  padding: 10px 20px;
+  border: none;
+`;
+
+export default Button;
